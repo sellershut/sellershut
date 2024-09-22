04095e6c3adefb74dffb677de7da93d273aa2481
rtkay <dev@kanjala.com>
2024-09-22 15:24:54 +0200

    feat: convert time to timestamp

--- crates/sellershut-core/src/lib.rs
+++ crates/sellershut-core/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#[cfg(feature = "users")]
+pub mod users;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[cfg(all(feature = "base", any(feature = "users")))]
+pub mod google {
+    pub mod protobuf {
+        include!(concat!(env!("OUT_DIR"), "/google.protobuf.rs"));
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        #[cfg(feature = "time")]
+        pub mod utils {
+
+            use crate::google::protobuf::Timestamp;
+            use time::{Error, OffsetDateTime};
+
+            pub fn to_offset_datetime(timestamp: Timestamp) -> Result<OffsetDateTime, Error> {
+                let seconds = timestamp.seconds;
+                let nanos = timestamp.nanos as i64;
+                let nanoseconds = nanos % 1_000_000_000;
+                let d = OffsetDateTime::from_unix_timestamp(seconds)?
+                    + time::Duration::nanoseconds(nanoseconds);
+                Ok(d)
+            }
+
+            pub fn to_timestamp(dt: OffsetDateTime) -> Timestamp {
+                Timestamp {
+                    seconds: dt.unix_timestamp(),
+                    nanos: dt.nanosecond() as i32,
+                }
+            }
+        }
     }
 }
