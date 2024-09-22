65ffc46ac1185ca6f1afb48781fd5980eb9a2dc8
rtkay <dev@kanjala.com>
2024-09-22 18:17:05 +0200

    feat: impl Actor

--- crates/sellershut-core/src/users/mod.rs
+++ crates/sellershut-core/src/users/mod.rs
@@ -1,4 +1,3 @@
 tonic::include_proto!("users");
 
-pub const USERS_DESCRIPTOR: &[u8] =
-    tonic::include_file_descriptor_set!("users_descriptor");
+pub const USERS_DESCRIPTOR: &[u8] = tonic::include_file_descriptor_set!("users_descriptor");
--- crates/sellershut/src/entities/user.rs
+++ crates/sellershut/src/entities/user.rs
@@ -1,32 +1,46 @@
+use std::str::FromStr;
+
 use crate::state::AppState;
-use activitypub_federation::{config::Data, fetch::object_id::ObjectId, traits::Object};
+use activitypub_federation::{
+    config::Data,
+    fetch::object_id::ObjectId,
+    kinds::actor::PersonType,
+    protocol::public_key::PublicKey,
+    traits::{Actor, Object},
+};
 use sellershut_core::users::{CreateUserRequest, User as DbUser};
 use serde::{Deserialize, Serialize};
 use tonic::IntoRequest;
+use tracing::{info_span, Instrument};
 use url::Url;
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct User(DbUser);
+pub struct LocalUser(DbUser);
 
-impl User {
+impl LocalUser {
     pub fn get(&self) -> &DbUser {
         &self.0
     }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct LocalUser {
-    id: ObjectId<User>,
+pub struct User {
+    #[serde(rename = "type")]
+    kind: PersonType,
+    preferred_username: String,
+    id: ObjectId<LocalUser>,
+    inbox: Url,
+    public_key: PublicKey,
 }
 
 #[tonic::async_trait]
-impl Object for User {
+impl Object for LocalUser {
     #[doc = " App data type passed to handlers. Must be identical to"]
     #[doc = " [crate::config::FederationConfigBuilder::app_data] type."]
     type DataType = AppState;
 
     #[doc = " The type of protocol struct which gets sent over network to federate this database struct."]
-    type Kind = LocalUser;
+    type Kind = User;
 
     #[doc = " Error type returned by handler methods"]
     type Error = tonic::Status;
@@ -35,11 +49,6 @@ impl Object for User {
     #[doc = ""]
     #[doc = " Should return `Ok(None)` if not found."]
     #[must_use]
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn read_from_id(
         object_id: Url,
         data: &Data<Self::DataType>,
@@ -52,13 +61,15 @@ impl Object for User {
     #[doc = " Called when a local object gets fetched by another instance over HTTP, or when an object"]
     #[doc = " gets sent in an activity."]
     #[must_use]
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn into_json(self, data: &Data<Self::DataType>) -> Result<Self::Kind, Self::Error> {
-        todo!()
+        let id: ObjectId<LocalUser> = ObjectId::from_str(&self.0.id).unwrap();
+        Ok(User {
+            preferred_username: self.0.username.clone(),
+            kind: Default::default(),
+            id,
+            inbox: Url::from_str(&self.0.inbox).unwrap(),
+            public_key: self.public_key(),
+        })
     }
 
     #[doc = " Verifies that the received object is valid."]
@@ -69,11 +80,6 @@ impl Object for User {
     #[doc = " It is necessary to use a separate method for this, because it might be used for activities"]
     #[doc = " like `Delete/Note`, which shouldn\'t perform any database write for the inner `Note`."]
     #[must_use]
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn verify(
         json: &Self::Kind,
         expected_domain: &Url,
@@ -88,11 +94,6 @@ impl Object for User {
     #[doc = " should write the received object to database. Note that there is no distinction between"]
     #[doc = " create and update, so an `upsert` operation should be used."]
     #[must_use]
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn from_json(json: Self::Kind, data: &Data<Self::DataType>) -> Result<Self, Self::Error> {
         let user = DbUser {
             id: json.id.to_string(),
@@ -104,11 +105,30 @@ impl Object for User {
 
         let response = client
             .create_user(request.into_request())
+            .instrument(info_span!("rpc-create-user"))
             .await?
             .into_inner()
             .user;
 
         let user = response.expect("user");
-        Ok(User(user))
+        Ok(LocalUser(user))
+    }
+}
+
+impl Actor for LocalUser {
+    fn id(&self) -> Url {
+        Url::from_str(&self.0.id).unwrap()
+    }
+
+    fn public_key_pem(&self) -> &str {
+        &self.0.public_key_pem
+    }
+
+    fn private_key_pem(&self) -> Option<String> {
+        self.0.private_key_pem.clone()
+    }
+
+    fn inbox(&self) -> Url {
+        Url::from_str(&self.0.inbox).unwrap()
     }
 }
