af78225056c080fd4de38ef9f2a5a9fa8bad79c3
rtkay123 <dev@kanjala.com>
2024-12-24 15:58:51 +0200

    chore(users): migrations

---
+++ crates/users-service/migrations/20241224135525_user.sql
@@ -0,0 +1,27 @@
+create table "user" (
+    id varchar(21) primary key,
+    username varchar(20) unique not null,
+    email varchar(255) unique not null,
+    avatar_url varchar,
+    inbox varchar not null,
+    public_key varchar not null,
+    private_key varchar,
+    local boolean not null,
+    followers varchar[] not null default '{}',
+    created_at timestamptz default current_timestamp not null,
+    last_refreshed_at timestamptz default current_timestamp not null,
+    updated_at timestamptz default current_timestamp not null
+);
+
+create or replace function update_updated_at()
+returns trigger as $$
+begin
+    new.updated_at = current_timestamp;
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger set_updated_at
+before update on "user"
+for each row
+execute function update_updated_at();
