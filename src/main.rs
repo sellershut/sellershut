55620fc982d696caa1f79c72f51e3130586ede52
rtkay123 <dev@kanjala.com>
2025-02-08 16:39:04 +0200

    feat: tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use sellershut_services::tracing::TracingBuilder;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let _tracing = TracingBuilder::new().build(None);
+    info!("Hello, world!");
+
+    Ok(())
 }
