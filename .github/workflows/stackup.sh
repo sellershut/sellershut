741b0673f04aabc77bf46b595cca53a6cfd55f08
rtkay123 <dev@kanjala.com>
2025-02-15 10:39:15 +0200

    ci: disable interactive mode

--- .github/workflows/stackup.sh
+++ .github/workflows/stackup.sh
@@ -10,7 +10,9 @@ something = "http://localhost:8080"
 [database]
 pool_size = 100
 port = 5432
-name = "users"
+name = "postgres"
 host = "localhost"
 user = "postgres"
 password = "password"' > users.toml
+
+docker run -d --network="host" -v ./users.toml:/users.toml ghcr.io/sellershut/users-service:master ./users-service --config-file "users.toml"
