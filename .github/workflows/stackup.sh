25cf37be1c683c5b7d1a4a73d968ee5ceb2d1df4
rtkay123 <dev@kanjala.com>
2025-02-22 11:05:23 +0200

    ci: start categories service

--- .github/workflows/stackup.sh
+++ .github/workflows/stackup.sh
@@ -10,9 +10,26 @@ something = "http://localhost:8080"
 [database]
 pool_size = 100
 port = 5432
-name = "postgres"
+name = "users"
 host = "localhost"
 user = "postgres"
 password = "password"' > users.toml
 
+echo '[application]
+env = "development"
+port = 1610
+log-level = "h2=info,debug"
+
+[misc]
+max_query_results = 1000
+
+[database]
+pool_size = 100
+port = 5432
+name = "categories"
+host = "localhost"
+user = "postgres"
+password = "password"' > categories.toml
+
 docker run -d --network="host" -v ./users.toml:/users.toml ghcr.io/sellershut/users-service:master ./users-service --config-file "users.toml"
+docker run -d --network="host" -v ./categories.toml:/categories.toml ghcr.io/sellershut/categories-service:master ./categories-service --config-file "categories.toml"
